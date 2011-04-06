c5e4481307f704ea88572381f1db42886b0e6a15
Chris Mytton <chrism@simpleweb.co.uk>
2011-04-06 17:27:27 +0100

    whitespace

--- js/jquery.multirowcheckboxmenu.js
+++ js/jquery.multirowcheckboxmenu.js
@@ -11,181 +11,181 @@
  * Please feel free to use this code in your projects, just respect the license and retain this header.
  */
 (function($){
-	function log() {
-		if (window.console && console.log) {
-			for (var i = 0, len = arguments.length; i < len; i++) {
-				console.log(arguments[i]);
-			}
-		}
-	};
-	$.fn.checkboxMenu = function(options) {
+  function log() {
+    if (window.console && console.log) {
+      for (var i = 0, len = arguments.length; i < len; i++) {
+        console.log(arguments[i]);
+      }
+    }
+  };
+  $.fn.checkboxMenu = function(options) {
 
-		var defaults = {
-			buttonSelector: ":submit",
-			checkboxIDPrefix: "selectAll",
-			checkboxClasses: "selectAll",
-			semiSelectClass: "semiSelect",
-			menuClass: "",
-                        menuLinkClass: "menu",
-			downClass: "down",
-			childCheckboxes: "input[type=checkbox]",
-                        removeSelectedItem: true,
-			menuItemClick: function() { return true; }
-		};
+    var defaults = {
+      buttonSelector: ":submit",
+      checkboxIDPrefix: "selectAll",
+      checkboxClasses: "selectAll",
+      semiSelectClass: "semiSelect",
+      menuClass: "",
+      menuLinkClass: "menu",
+      downClass: "down",
+      childCheckboxes: "input[type=checkbox]",
+      removeSelectedItem: true,
+      menuItemClick: function() { return true; }
+    };
 
-		var opts = $.extend(defaults, options);
-		var containerCount = 0;
+    var opts = $.extend(defaults, options);
+    var containerCount = 0;
 
-		return this.each(function(){
+    return this.each(function(){
 
-			var checkboxMenuContainer = $(this);
-			checkboxMenuContainer.addClass('jquery-multirow-checkbox-menu-container');
+      var checkboxMenuContainer = $(this);
+      checkboxMenuContainer.addClass('jquery-multirow-checkbox-menu-container');
 
-			//UL containing the menu.
-			var dropdownMenu = $("<ul>")
-                            .appendTo(checkboxMenuContainer)
-                            .addClass(opts.menuClass)
-                            .addClass('jquery-multirow-checkbox-menu');
+      //UL containing the menu.
+      var dropdownMenu = $("<ul>")
+      .appendTo(checkboxMenuContainer)
+      .addClass(opts.menuClass)
+      .addClass('jquery-multirow-checkbox-menu');
 
-                        var selectedItem = false;
+      var selectedItem = false;
 
-			//If there are input buttons inside the container, these will become the menu items.
-			if($(opts.buttonSelector, checkboxMenuContainer).length >0) {
-
-
-				//Build the menu
-				//Loop over each submit button in our container and change it in to an LI item.
-				$(opts.buttonSelector, checkboxMenuContainer).each(function() {
-
-					var submitButton = $(this);
-					var buttonText = $(this).attr('value');
-                                        var itemIsSelected = submitButton.hasClass('selected');
-                                        if(itemIsSelected) {
-                                            selectedItem = buttonText;
-                                        }
-
-                                        //If selected item, should it be included in menu?
-                                        if(!itemIsSelected || !opts.removeSelectedItem) {
-
-                                            var li = $("<li>")
-                                                    .appendTo(dropdownMenu)
-                                                    .text(buttonText)
-                                                    .click(function() {
-
-                                                            var clickReturn = false;
-                                                            if(selectedItem) {
-                                                                clickReturn = opts.menuItemClick.call(this, $(this).text());
-                                                            } else {
-                                                                clickReturn = opts.menuItemClick.call(this, $(this).text(), $(opts.childCheckboxes).filter(':checked').not(checkboxSelectAll).length);
-                                                            }
-
-                                                            if(clickReturn) {
-
-                                                                    //Hide menus on submit, makes it feel like something has happened on click.
-                                                                    $('.jquery-multirow-checkbox-menu').hide();
-
-                                                                    //Put hidden element in to the form that will provide the same value as if the button had been pushed.
-                                                                    var hiddenVal = $('<input>').attr('type','hidden')
-                                                                                                                    .attr('name', submitButton.attr('name'))
-                                                                                                                    .attr('value',submitButton.attr('value'))
-                                                                                                                    .appendTo($(this).parents('form'));
-                                                                    $(this).parents('form').submit();
-                                                            }
-                                                            return false;
-
-                                                    });
-
-                                            //Copy over ID of button to LI.
-                                            if(submitButton.attr('id')) {
-                                                    li.attr('id',submitButton.attr('id'));
-                                            }
-
-                                            //Copy over Classes of button to LI.
-                                            if(submitButton.attr('class')) {
-                                                    li.attr('class',submitButton.attr('class'));
-                                            }
-
-                                        }
-
-					submitButton.remove(); //Take out the submit button.
-				});
-
-				//Add the menu link to show the menu items.
-				var menuLink = $('<a>').attr('href','#')
-					.addClass(opts.menuLinkClass)
-					.text('Menu')
-					.insertBefore(dropdownMenu)
-					.click(function() {
-
-                                                //Hide any other menus from the same plugin that might not be hidden.
-                                                $('.jquery-multirow-checkbox-menu').not(dropdownMenu).hide();
-                                                $('.jquery-multirow-checkbox-menu-container').not(checkboxMenuContainer).removeClass(opts.downClass);
-						dropdownMenu.toggle();
-						checkboxMenuContainer.toggleClass(opts.downClass);
-
-						//Must return false otherwise click bubbles to document click below.
-						return false;
-					});
-
-				//Attach an event to document click that will hide the menu if anything else is clicked.
-				$('body').click(function(){
-                                    $('.jquery-multirow-checkbox-menu').hide();
-                                    $('.jquery-multirow-checkbox-menu-container').removeClass(opts.downClass);
-				});
-			}
-
-                        if(selectedItem) {
-
-                            var selectedSpan = $('<span>').addClass('selectedItem').text(selectedItem).insertBefore(menuLink);
-
-                        } else {
-
-                            //Label around the checkbox.
-                            var checkboxLabel = $('<label>').attr('for', opts.checkboxIDPrefix + containerCount)
-									.prependTo(checkboxMenuContainer);
-
-                            //The select all checkbox.
-                            var checkboxSelectAll = $('<input>').attr('id',opts.checkboxIDPrefix + containerCount)
-                                                                                    .attr('type', 'checkbox')
-                                                                                    .addClass(opts.checkboxClasses)
-                                                                                    .appendTo(checkboxLabel);
-
-                            //Attach the event to the main checkbox that will toggle All/None selected check boxes.
-                            checkboxSelectAll.click(function() {
-
-                                //If the item is semi selected then when we click, always turn it on.
-                                if(checkboxSelectAll.hasClass(opts.semiSelectClass)) {
-                                    checkboxSelectAll.attr('checked',true);
-                                }
-
-                                $(opts.childCheckboxes).each(function() {
-                                        $(this).attr('checked', checkboxSelectAll.attr('checked'));
-                                });
-                                checkboxSelectAll.removeClass(opts.semiSelectClass);
-                            });
-
-                            //Attach event to all child boxes which will set the semi-selected transparency on the main check box.
-                            $(opts.childCheckboxes).not(checkboxSelectAll).click(function() {
-
-                                    if($(opts.childCheckboxes).filter(':checked').not(checkboxSelectAll).length != $(opts.childCheckboxes).not(checkboxSelectAll).length && $(opts.childCheckboxes).filter(':checked').not(checkboxSelectAll).length != 0) {
-                                            checkboxSelectAll.addClass(opts.semiSelectClass);
-                                            checkboxSelectAll.attr('checked',true);
-                                    } else {
-                                            checkboxSelectAll.removeClass(opts.semiSelectClass);
-
-                                            //If everything is checked, check the master as well.
-                                            if($(opts.childCheckboxes).filter(':checked').not(checkboxSelectAll).length != 0) {
-                                                    checkboxSelectAll.attr('checked',true);
-                                            } else {
-                                                checkboxSelectAll.attr('checked',false);
-                                            }
-                                    }
-
-                            });
-
-                        }
+      //If there are input buttons inside the container, these will become the menu items.
+      if($(opts.buttonSelector, checkboxMenuContainer).length >0) {
 
 
-		});
-	};
+        //Build the menu
+        //Loop over each submit button in our container and change it in to an LI item.
+        $(opts.buttonSelector, checkboxMenuContainer).each(function() {
+
+          var submitButton = $(this);
+          var buttonText = $(this).attr('value');
+          var itemIsSelected = submitButton.hasClass('selected');
+          if(itemIsSelected) {
+            selectedItem = buttonText;
+          }
+
+          //If selected item, should it be included in menu?
+          if(!itemIsSelected || !opts.removeSelectedItem) {
+
+            var li = $("<li>")
+            .appendTo(dropdownMenu)
+            .text(buttonText)
+            .click(function() {
+
+              var clickReturn = false;
+              if(selectedItem) {
+                clickReturn = opts.menuItemClick.call(this, $(this).text());
+              } else {
+                clickReturn = opts.menuItemClick.call(this, $(this).text(), $(opts.childCheckboxes).filter(':checked').not(checkboxSelectAll).length);
+              }
+
+              if(clickReturn) {
+
+                //Hide menus on submit, makes it feel like something has happened on click.
+                $('.jquery-multirow-checkbox-menu').hide();
+
+                //Put hidden element in to the form that will provide the same value as if the button had been pushed.
+                var hiddenVal = $('<input>').attr('type','hidden')
+                .attr('name', submitButton.attr('name'))
+                .attr('value',submitButton.attr('value'))
+                .appendTo($(this).parents('form'));
+                $(this).parents('form').submit();
+              }
+              return false;
+
+            });
+
+            //Copy over ID of button to LI.
+            if(submitButton.attr('id')) {
+              li.attr('id',submitButton.attr('id'));
+            }
+
+            //Copy over Classes of button to LI.
+            if(submitButton.attr('class')) {
+              li.attr('class',submitButton.attr('class'));
+            }
+
+          }
+
+          submitButton.remove(); //Take out the submit button.
+        });
+
+        //Add the menu link to show the menu items.
+        var menuLink = $('<a>').attr('href','#')
+        .addClass(opts.menuLinkClass)
+        .text('Menu')
+        .insertBefore(dropdownMenu)
+        .click(function() {
+
+          //Hide any other menus from the same plugin that might not be hidden.
+          $('.jquery-multirow-checkbox-menu').not(dropdownMenu).hide();
+          $('.jquery-multirow-checkbox-menu-container').not(checkboxMenuContainer).removeClass(opts.downClass);
+          dropdownMenu.toggle();
+          checkboxMenuContainer.toggleClass(opts.downClass);
+
+          //Must return false otherwise click bubbles to document click below.
+          return false;
+        });
+
+        //Attach an event to document click that will hide the menu if anything else is clicked.
+        $('body').click(function(){
+          $('.jquery-multirow-checkbox-menu').hide();
+          $('.jquery-multirow-checkbox-menu-container').removeClass(opts.downClass);
+        });
+      }
+
+      if(selectedItem) {
+
+        var selectedSpan = $('<span>').addClass('selectedItem').text(selectedItem).insertBefore(menuLink);
+
+      } else {
+
+        //Label around the checkbox.
+        var checkboxLabel = $('<label>').attr('for', opts.checkboxIDPrefix + containerCount)
+          .prependTo(checkboxMenuContainer);
+
+        //The select all checkbox.
+        var checkboxSelectAll = $('<input>').attr('id',opts.checkboxIDPrefix + containerCount)
+        .attr('type', 'checkbox')
+        .addClass(opts.checkboxClasses)
+        .appendTo(checkboxLabel);
+
+        //Attach the event to the main checkbox that will toggle All/None selected check boxes.
+        checkboxSelectAll.click(function() {
+
+          //If the item is semi selected then when we click, always turn it on.
+          if(checkboxSelectAll.hasClass(opts.semiSelectClass)) {
+            checkboxSelectAll.attr('checked',true);
+          }
+
+          $(opts.childCheckboxes).each(function() {
+            $(this).attr('checked', checkboxSelectAll.attr('checked'));
+          });
+          checkboxSelectAll.removeClass(opts.semiSelectClass);
+        });
+
+        //Attach event to all child boxes which will set the semi-selected transparency on the main check box.
+        $(opts.childCheckboxes).not(checkboxSelectAll).click(function() {
+
+          if($(opts.childCheckboxes).filter(':checked').not(checkboxSelectAll).length != $(opts.childCheckboxes).not(checkboxSelectAll).length && $(opts.childCheckboxes).filter(':checked').not(checkboxSelectAll).length != 0) {
+            checkboxSelectAll.addClass(opts.semiSelectClass);
+            checkboxSelectAll.attr('checked',true);
+          } else {
+            checkboxSelectAll.removeClass(opts.semiSelectClass);
+
+            //If everything is checked, check the master as well.
+            if($(opts.childCheckboxes).filter(':checked').not(checkboxSelectAll).length != 0) {
+              checkboxSelectAll.attr('checked',true);
+            } else {
+              checkboxSelectAll.attr('checked',false);
+            }
+          }
+
+        });
+
+      }
+
+
+    });
+  };
 })(jQuery);
